cfb6b85ff1f367a15e426b60ef5c7fba8ad5374d
Romalutsiv <lutsiv55@gmail.com>
2020-02-04 15:46:34 +0200

    Initial project add

---
+++ src/main/java/com/romychdev/sfppetclinic/SfpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.romychdev.sfppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romychdev/sfppetclinic/SfpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romychdev.sfppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
